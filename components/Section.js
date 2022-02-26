0ce95ecbcf0d81ecad10ab58c7ea88c65ee1e565
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-26 18:24:10 -0500

    map over videos

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,17 @@
+import Card from './Card';
+
 const Section = ({ genre, videos }) => {
-  console.log(videos)  
   return (
-    <h3>{genre}</h3>
+    <div className={"section"}>
+       <h3>{genre}</h3>
+       <div className="video-feed">
+         {videos.map(video => (
+             <a key={video.id} href={`/video/${video.slug}`}>
+               <Card thumbnail={video.thumbnail} />
+             </a>
+         ))}
+       </div>
+    </div>
   )
 }
 
